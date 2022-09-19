60b72a9597ba1d37a3aa44bce2c0898072ca2bb1
shubh-verma <shubhvcs@gmail.com>
2022-09-20 03:35:45 +0530

    [Add] error handler files

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,14 @@ const dotenv = require("dotenv");
 
 const connectDatabase = require("./config/database");
 
+//Handling Uncaught Exception
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down server due to uncaught exception`);
+  process.exit(1);
+});
+
 // config
 dotenv.config({ path: "backend/config/config.env" });
 
@@ -12,4 +20,14 @@ connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promis Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down server due to unhandled promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
