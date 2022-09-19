957c376b3a46cb32ba2e768d5c68504339a90018
shubh-verma <shubhvcs@gmail.com>
2022-09-20 05:17:39 +0530

    [Add] Authentication init features

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,9 @@ app.use(express.json());
 
 //Route Imports
 const product = require("./routes/productRoute");
-
+const user = require("./routes/userRoute");
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 //Middleware for error
 app.use(errorMiddleware);
