26d4df7e7ca48afb55869695ebd418cb63540fb8
shubh-verma <shubhvcs@gmail.com>
2022-10-17 20:56:17 +0530

    [Add] Product File

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
+import Product from "./Product.js";
+
+const product = {
+  name: "Blue T-shirt",
+  images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+  price: "Rs.3000",
+  _id: "abhi",
+};
 
 const Home = () => {
   return (
@@ -14,6 +22,11 @@ const Home = () => {
             Scroll <CgMouse />
           </button>
         </a>
+      </div>
+      <h2 className="homeHeading">Feature Products</h2>
+
+      <div className="container" id="container">
+        <Product product={product} />
       </div>
     </Fragment>
   );
