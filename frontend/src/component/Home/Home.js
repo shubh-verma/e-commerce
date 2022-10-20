42e3ea4419affa16cdbb7e7a67b2691617d2eefc
shubh-verma <shubhvcs@gmail.com>
2022-10-20 23:39:19 +0530

    [Add] productReducers and productConstants

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from "./Product.js";
+import MetaData from "../layout/MetaData";
 
 const product = {
   name: "Blue T-shirt",
@@ -13,6 +14,7 @@ const product = {
 const Home = () => {
   return (
     <Fragment>
+      <MetaData title={"Ecommerce"} />
       <div className="banner">
         <p>Welcome to Ecommerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -6,20 +6,20 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer id="footer">
-      <div class="leftFooter">
+      <div className="leftFooter">
         <h4> Download Our App</h4>
         <p>Download Our App form Android and IOS phone</p>
         <img src={playStore} alt="playstore"></img>
         <img src={appStore} alt="Appstore"></img>
       </div>
 
-      <div class="midFooter">
+      <div className="midFooter">
         <h1>E-Commerce</h1>
         <p>High Quality is my first Priority</p>
         <p>Copyrights 2022 &copy; ShubhamVerma</p>
       </div>
 
-      <div class="rightFooter">
+      <div className="rightFooter">
         <h4>Follow me</h4>
         <a href="https://github.com/shubh-verma">Github</a>
         <a href="https://www.instagram.com/shubh_vrma/">Instagram</a>
