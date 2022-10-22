d8278798d3c7bd8deab01cef977673f8ec40980a
shubh-verma <shubhvcs@gmail.com>
2022-10-22 19:23:03 +0530

    Minor Push

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,8 +1,10 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from "./Product.js";
 import MetaData from "../layout/MetaData";
+import { getProduct } from "../../action/productAction";
+import { useDispatch } from "react-redux";
 
 const product = {
   name: "Blue T-shirt",
@@ -12,6 +14,12 @@ const product = {
 };
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
+
   return (
     <Fragment>
       <MetaData title={"Ecommerce"} />
