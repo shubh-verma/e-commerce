23a0769c28158e91c1d0ee882f6a120bf1f227ee
shubh-verma <shubhvcs@gmail.com>
2022-10-31 19:28:40 +0530

    [Add] Search

---
+++ frontend/src/component/Product/Products.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useEffect, useState} from 'react';
+import "./Products.css";
+import { useSelector, useDispatch } from 'react-redux';
+import { clearErrors, getProduct } from '../../action/productAction';
+import Loader from '../layout/Loader/Loader';
+import ProductCard from '../Home/ProductCard';
+import { useAlert } from "react-alert";
+const Products = ({ match }) => {
+    const dispatch = useDispatch();
+    const alert = useAlert();
+
+    const {
+        products,
+        loading,
+        error,
+        productsCount,
+        resultPerPage,
+        filteredProductsCount,
+      } = useSelector((state) => state.products);
+
+      const keyword = match.params.keyword;
+
+
+    useEffect(() => {
+        if (error) {
+          alert.error(error);
+          dispatch(clearErrors());
+        }
+        dispatch(getProduct(keyword));
+      }, [dispatch, keyword, alert, error]);
+  return (
+    <Fragment>
+        {loading ? <Loader /> : <Fragment>
+            <h2 className="productsHeading">Products</h2>
+            <div className="products">
+                {products && products.map((product) => (
+                <ProductCard key={product._id} product={product} />))}
+            </div>
+        </Fragment>}
+    </Fragment>
+  )
+}
+
+export default Products;
