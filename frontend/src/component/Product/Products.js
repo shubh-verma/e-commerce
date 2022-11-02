647cd5d59e7702af05da96c70d108ec2e7510f3c
shubh-verma <shubhvcs@gmail.com>
2022-11-02 21:40:46 +0530

    Added price filter functionality

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,49 +1,74 @@
-import React, { Fragment, useEffect, useState} from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 import "./Products.css";
-import { useSelector, useDispatch } from 'react-redux';
-import { clearErrors, getProduct } from '../../action/productAction';
-import Loader from '../layout/Loader/Loader';
-import ProductCard from '../Home/ProductCard';
+import { useSelector, useDispatch } from "react-redux";
+import { clearErrors, getProduct } from "../../action/productAction";
+import Loader from "../layout/Loader/Loader";
+import ProductCard from "../Home/ProductCard";
 import Pagination from "react-js-pagination";
-
+import Slider from "@material-ui/core/Slider";
 import { useAlert } from "react-alert";
+import Typography from "@material-ui/core/Typography";
+
 const Products = ({ match }) => {
-    const dispatch = useDispatch();
-    const [currentPage, setCurrentPage] = useState(1);
-    const alert = useAlert();
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 25000]);
+  //const [category, setCategory] = useState("");
+  const alert = useAlert();
 
-    const {
-        products,
-        loading,
-        error,
-        productsCount,
-        resultPerPage,
-        filteredProductsCount,
-      } = useSelector((state) => state.products);
+  const {
+    products,
+    loading,
+    error,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
 
-      const keyword = match.params.keyword;
-      const setCurrentPageNo = (e) => {
-        setCurrentPage(e);
-      }; 
+  const keyword = match.params.keyword;
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
 
-    useEffect(() => {
-        if (error) {
-          alert.error(error);
-          dispatch(clearErrors());
-        }
-        dispatch(getProduct(keyword, currentPage));
-      }, [dispatch, keyword, currentPage, alert, error]);
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProduct(keyword, currentPage, price));
+  }, [dispatch, keyword, currentPage, alert, error, price]);
   return (
     <Fragment>
-        {loading ? <Loader /> : <Fragment>
-            <h2 className="productsHeading">Products</h2>
-            <div className="products">
-                {products && products.map((product) => (
-                <ProductCard key={product._id} product={product} />))}
-            </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <h2 className="productsHeading">Products</h2>
+          <div className="products">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
 
-            {resultPerPage < productsCount && (
-              <div className="paginationBox">
+          <div className="filterBox">
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              min={0}
+              max={25000}
+            />
+          </div>
+
+          {resultPerPage < productsCount && (
+            <div className="paginationBox">
               <Pagination
                 activePage={currentPage}
                 itemsCountPerPage={resultPerPage}
@@ -59,10 +84,11 @@ const Products = ({ match }) => {
                 activeLinkClass="pageLinkActive"
               />
             </div>
-            )}
-        </Fragment>}
+          )}
+        </Fragment>
+      )}
     </Fragment>
-  )
-}
+  );
+};
 
 export default Products;
