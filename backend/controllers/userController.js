4b88d9590657289ce119dd4959a1f40450926d49
shubh-verma <shubhvcs@gmail.com>
2022-11-04 18:36:40 +0530

    [Add] Cloudinary

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,18 +3,25 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/JWTToken");
 const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
+const cloudinary = require("cloudinary");
 
 //Register a User
 
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "avatar",
+    width: 150,
+    crop: "scale",
+  });
   const { name, email, password } = req.body;
   const user = await User.create({
     name,
     email,
     password,
     avatar: {
-      public_id: "this is a sample_id",
-      url: "profilePicUrl",
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
     },
   });
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const cloudinary = require("cloudinary");
 
 const connectDatabase = require("./config/database");
 
@@ -17,6 +18,12 @@ dotenv.config({ path: "backend/config/config.env" });
 
 //connecting to Database
 connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
