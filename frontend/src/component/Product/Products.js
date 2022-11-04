d7a863cd6303319968a2ad98621bb33f8efdeccb
shubh-verma <shubhvcs@gmail.com>
2022-11-04 09:28:52 +0530

    [Add] metadata title

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -8,6 +8,7 @@ import Pagination from "react-js-pagination";
 import Slider from "@material-ui/core/Slider";
 import { useAlert } from "react-alert";
 import Typography from "@material-ui/core/Typography";
+import MetaData from "../layout/MetaData";
 
 const categories = [
   "Laptop",
@@ -60,6 +61,7 @@ const Products = ({ match }) => {
         <Loader />
       ) : (
         <Fragment>
+          <MetaData title="PRODUCTS -- ECOMMERCE" />
           <h2 className="productsHeading">Products</h2>
           <div className="products">
             {products &&
