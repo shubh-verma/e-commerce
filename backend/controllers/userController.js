143ef98eb5ff18cdb777c18202616f9dd49c6b07
shubh-verma <shubhvcs@gmail.com>
2022-11-07 15:46:28 +0530

    [Add] resetPassword with token generated via email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,9 +77,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  const resetPasswordUrl = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/password/reset/${resetToken}`;
+  const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
 
   const message = `Your Password reset token is :- \n\n${resetPasswordUrl} \n\n If you have not requested this email then, please ignore it`;
 
--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -21,6 +21,9 @@ import {
   FORGOT_PASSWORD_REQUEST,
   FORGOT_PASSWORD_SUCCESS,
   FORGOT_PASSWORD_FAIL,
+  RESET_PASSWORD_SUCCESS,
+  RESET_PASSWORD_REQUEST,
+  RESET_PASSWORD_FAIL,
   CLEAR_ERRORS,
 } from "../constants/userConstants";
 
@@ -121,6 +124,7 @@ export const profileReducer = (state = { user: {} }, action) => {
 export const forgotPasswordReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case FORGOT_PASSWORD_REQUEST:
+    case RESET_PASSWORD_REQUEST:
       return {
         ...state,
         loading: true,
@@ -132,7 +136,14 @@ export const forgotPasswordReducer = (state = { user: {} }, action) => {
         loading: false,
         message: action.payload,
       };
+    case RESET_PASSWORD_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: action.payload,
+      };
     case FORGOT_PASSWORD_FAIL:
+    case RESET_PASSWORD_FAIL:
       return {
         ...state,
         loading: false,
