93bfcbadf872085bc1a0f093cb97526d44bc901e
shubh-verma <shubhvcs@gmail.com>
2022-11-08 15:04:07 +0530

    [Add] Cart module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import UpdateProfile from "./component/User/UpdateProfile.js";
 import UpdatePassword from "./component/User/UpdatePassword.js";
 import ForgotPassword from "./component/User/ForgotPassword.js";
 import ResetPassword from "./component/User/ResetPassword.js";
+import Cart from "./component/Cart/Cart.js";
 
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
@@ -55,6 +56,7 @@ function App() {
           element={<ResetPassword />}
         />
         <Route exact path="/login" element={<LogInSignUp />} />
+        <Route exact path="/cart" element={<Cart />} />
       </Routes>
 
       <Footer />
--- frontend/src/action/cartAction.js
+++ frontend/src/action/cartAction.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../constants/cartConstants";
+import { ADD_TO_CART, REMOVE_CART_ITEM } from "../constants/cartConstants";
 import axios from "axios";
 
 // Add to Cart
@@ -15,6 +15,16 @@ export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
       stock: data.product.Stock,
       quantity,
     },
+  });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+// Remove from Cart
+
+export const removeItemsFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: REMOVE_CART_ITEM,
+    payload: id,
   });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
