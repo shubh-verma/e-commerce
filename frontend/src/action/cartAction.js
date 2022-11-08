d094399000bd64751b3aed74e7ff2b110dc483d8
shubh-verma <shubhvcs@gmail.com>
2022-11-08 17:12:07 +0530

    [Add] save shipping info

--- frontend/src/action/cartAction.js
+++ frontend/src/action/cartAction.js
@@ -1,4 +1,8 @@
-import { ADD_TO_CART, REMOVE_CART_ITEM } from "../constants/cartConstants";
+import {
+  ADD_TO_CART,
+  REMOVE_CART_ITEM,
+  SAVE_SHIPPING_INFO,
+} from "../constants/cartConstants";
 import axios from "axios";
 
 // Add to Cart
@@ -27,4 +31,14 @@ export const removeItemsFromCart = (id) => async (dispatch, getState) => {
     payload: id,
   });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+// Save Shipping Information
+
+export const saveShippingInfo = (data) => async (dispatch) => {
+  dispatch({
+    type: SAVE_SHIPPING_INFO,
+    payload: data,
+  });
+  localStorage.setItem("shippingInfo", JSON.stringify(data));
 };
