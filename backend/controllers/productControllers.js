c7a779e90fa3320cac772a3742fd4b6cb567b93b
shubh-verma <shubhvcs@gmail.com>
2022-11-12 04:49:25 +0530

    [Add] delete func for cloudinary images

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -113,6 +113,11 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Product Not Found", 404));
   }
 
+  // Deleting Images From Cloudinary
+  for (let i = 0; i < product.images.length; i++) {
+    await cloudinary.v2.uploader.destroy(product.images[i].public_id);
+  }
+
   await product.remove();
   res.status(200).json({
     success: true,
